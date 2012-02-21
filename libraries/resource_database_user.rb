4f19c34014aee0c706c536a828455e14273bd92f
Nikita Simakov <everm1nd.mail@gmail.com>
2012-02-21 19:20:17 +0400

    database_user resource refactoring

--- libraries/resource_database_user.rb
+++ libraries/resource_database_user.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,54 +36,12 @@ class Chef
         @action = :create
       end
 
-      def database_name(arg=nil)
-        set_or_return(
-          :database_name,
-          arg,
-          :kind_of => String
-        )
-      end
-
-      def username(arg=nil)
-        set_or_return(
-          :username,
-          arg,
-          :kind_of => String
-        )
-      end
-
-      def password(arg=nil)
-        set_or_return(
-          :password,
-          arg,
-          :kind_of => String,
-          :required => true
-        )
-      end
-
-      def table(arg=nil)
-        set_or_return(
-          :table,
-          arg,
-          :kind_of => String
-        )
-      end
-
-      def host(arg=nil)
-        set_or_return(
-          :host,
-          arg,
-          :kind_of => String
-        )
-      end
-
-      def privileges(arg=nil)
-        set_or_return(
-          :privileges,
-          arg,
-          :kind_of => Array
-        )
-      end
+      attribute :database_name, :kind_of => String, :default => nil
+      attribute :username, :kind_of => String, :default => nil
+      attribute :password, :kind_of => String, :default => nil, :required => true
+      attribute :table, :kind_of => String, :default => nil
+      attribute :host, :kind_of => String, :default => nil
+      attribute :privileges, :kind_of => String, :default => nil
 
     end
   end
