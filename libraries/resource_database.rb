997bc85ae898927e6fa7b2cf8ac67353c5674929
Nikita Simakov <everm1nd.mail@gmail.com>
2012-02-21 19:16:24 +0400

    database resource refactoring

--- libraries/resource_database.rb
+++ libraries/resource_database.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,82 +30,15 @@ class Chef
         @action = :create
       end
 
-      def database_name(arg=nil)
-        set_or_return(
-          :database_name,
-          arg,
-          :kind_of => String
-        )
-      end
+      attribute :database_name, :kind_of => String, :default => nil
+      attribute :connection, :required => true
+      attribute :sql, :kind_of => String, :default => nil
+      attribute :template, :kind_of => String, :default => "DEFAULT"
+      attribute :encoding, :kind_of => String, :default => "DEFAULT"
+      attribute :tablespace, :kind_of => String, :default => "DEFAULT"
+      attribute :connection_limit, :kind_of => String, :default => "-1"
+      attribute :owner, :kind_of => String, :default => nil
 
-      def connection(arg=nil)
-        set_or_return(
-          :connection,
-          arg,
-          :required => true
-        )
-      end
-
-      def sql(arg=nil)
-        set_or_return(
-          :sql,
-          arg,
-          :kind_of => String
-        )
-      end
-
-      def sql(arg=nil)
-        set_or_return(
-          :sql,
-          arg,
-          :kind_of => String
-        )
-      end
-
-      # 
-      def template(arg=nil)
-        set_or_return(
-          :template,
-          arg,
-          :kind_of => String,
-          :default => 'DEFAULT'
-        )
-      end
-
-      def encoding(arg=nil)
-        set_or_return(
-          :encoding,
-          arg,
-          :kind_of => String,
-          :default => 'DEFAULT'
-        )
-      end
-
-      def tablespace(arg=nil)
-        set_or_return(
-          :tablespace,
-          arg,
-          :kind_of => String,
-          :default => 'DEFAULT'
-        )
-      end
-
-      def connection_limit(arg=nil)
-        set_or_return(
-          :connection_limit,
-          arg,
-          :kind_of => String,
-          :default => '-1'
-        )
-      end
-
-      def owner(arg=nil)
-        set_or_return(
-          :owner,
-          arg,
-          :kind_of => String
-        )
-      end
     end
   end
 end
